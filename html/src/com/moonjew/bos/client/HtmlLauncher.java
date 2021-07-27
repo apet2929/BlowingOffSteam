82f2a0a4ff5ac72a2bbf32c1cb8adf1d33c01372
apet2929 <56840582+apet2929@users.noreply.github.com>
2021-07-27 17:35:22 -0500

    Initial Commit

---
+++ html/src/com/moonjew/bos/client/HtmlLauncher.java
@@ -0,0 +1,22 @@
+package com.moonjew.bos.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.moonjew.bos.BlowingOffSteam;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                // Resizable application, uses available space in browser
+                return new GwtApplicationConfiguration(true);
+                // Fixed size application:
+                //return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new BlowingOffSteam();
+        }
+}
